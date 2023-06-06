28edf688bf1a7ee63733452040a85d58134b704a
Nol3Dev <allanb2805@gmail.com>
2023-06-06 09:17:50 -0300

    add input NewTask

--- src/pages/NewTask/index.js
+++ src/pages/NewTask/index.js
@@ -1,11 +1,37 @@
-import React from "react"
-import { View, Text } from "react-native"
+import React, { useState } from "react"
+import { View, Text, TextInput, TouchableOpacity } from "react-native"
 
+import database from "../../config/firebaseconfig.js"
+import styles from "./style.js"
 
 export default function NewTask(){
+  const [description,setDescription] = useState(null)
+
+  function addTask(){
+    database.collection("Tasks").add({
+      descriprtion:description,
+      status: false
+    })
+    navigation.navigate("Task")
+  }
+
     return(
-        <View>
-          <Text>Page NewTask</Text>
+        <View style={styles.container}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput
+            style={styles.inputText}
+            placeholder="Ex: Estudar Javascript"
+            onChangeText={setDescription}
+            value={description}
+          />
+          <TouchableOpacity 
+            style={styles.buttonNewTask}
+            onPress={()=> {
+              addTask()
+            }}
+          >
+            <Text style={styles.iconButton}>Save</Text>
+          </TouchableOpacity>
         </View>
     )
 }
---
+++ src/pages/NewTask/style.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+
+    },
+
+    label: {
+        width: "90%",
+        marginTop: 20,
+        fontSize: 16,
+        marginLeft: 20,
+        color: "#f92e6a"
+    },
+
+    input: {
+        width: "90%",
+        marginTop: 10,
+        padding: 10,
+        height: 50,
+        borderBottonWidth: 1,
+        borderBottonColor: "#f92e6a",
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+});
+
+export default styles
