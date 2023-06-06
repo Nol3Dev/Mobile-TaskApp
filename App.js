b17638a6be8eab94ec46b034c654a258036499c3
Nol3Dev <allanb2805@gmail.com>
2023-06-05 23:48:09 -0300

    Create routes and navigation

--- App.js
+++ App.js
@@ -2,13 +2,37 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator} from '@react-navigation/stack';
 
+import Task from './src/pages/Task/index.js';
+import NewTask from './src/pages/NewTask/index.js';
+import Details from './src/pages/Details/index.js';
+
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Task">
-
+        <Stack.Screen
+          name="Task"
+          component={Task}
+          options={{
+            headerTintColor:"#f92e6a"
+          }}
+        />
+        <Stack.Screen
+          name="New Task"
+          component={NewTask}
+          options={{
+            headerTintColor:"#f92e6a"
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            headerTintColor:"#f92e6a"
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
